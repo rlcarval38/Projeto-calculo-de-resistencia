cba36e6543517fe15eeae09c9f0db0e9e674a25b
Roberto de Carvalho <116233666+rlcarval38@users.noreply.github.com>
2022-12-01 23:51:55 -0300

    update Readme
    Terminado o JavaScript e atualizado css

--- script/script.js
+++ script/script.js
@@ -1,16 +1,31 @@
+
+
 let n1nominal = document.getElementById('numnominal')
 let n1real = document.getElementById('numreal')
-let res1 = document.getElementById('res')   
+let res1 = document.getElementById('res')
+let cento = Number(0)  
     
 function clicar(){
 
     if (n1nominal.value.length == 0 || n1real.value.length == 0 && n1nominal.value == 0 || n1real.value == 0) {
         window.alert ('[ERRO] Verifique os dados digitados e tente novamente!')
+    }else if (n1real.value < n1nominal.value){
+        cento = (((n1nominal.value/n1real.value)-1)*100)
+
+        res1.innerHTML = `valores Digitados: Nominal: ${n1nominal.value}Ω Real ${n1real.value}Ω Resultado: ${cento.toFixed(2)} % Negativo`
     }else{
-        res1.innerHTML='correto'
+        cento = (((n1real.value/n1nominal.value)-1)*100)
+        
+        res1.innerHTML = `valores Digitados: Nominal: ${n1nominal.value}Ω  Real ${n1real.value}Ω Resultado: ${cento.toFixed(2)} % Positivo`
     }
+        n1real.value = ``
+        n1nominal.value = ``
+        n1nominal.focus() 
+}   
+        
+        
+    
+        
 
 
 
-
-}
